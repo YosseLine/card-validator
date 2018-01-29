e8043787be41ae3004611d37ac127209da00e2ea
YosseLine <yoseline_96@hotmail.com>
2018-01-29 18:19:38 -0500

    Funcionalidad

---
+++ src/prueba-index.js
@@ -0,0 +1,12 @@
+window.addEventListener('load', () =>{
+
+function validateName (id) {
+  for(let user in data ){
+    if(user === 'user') {
+      for (let eachName in data) {
+        console.log(user);
+      }
+    }
+  };
+}
+});
